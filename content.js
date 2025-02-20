e4fbea17009469c6f2d98689b2905b22901e3552
Nikita Semenov <bascetbol333@gmail.com>
2025-02-20 18:20:06 +0300

    add c4355.com, refactor and optimized

--- content.js
+++ content.js
@@ -2,20 +2,22 @@
  * Функция для замены флагов, ссылок и текста на странице.
  */
 function replaceFlagsAndEditLinks() {
-    // Замена класса "country-sanctioned" на "country-116"
-    document.querySelectorAll('.country-sanctioned').forEach(element => {
-        element.classList.remove('country-sanctioned');
-        element.classList.add('country-116');
-    });
+  document
+    .querySelectorAll('a[href*="on-the-invasion-of-ukraine"]')
+    .forEach((element) => {
+      // Замена класса "country-sanctioned" (серый флаг) на "country-116" (флаг России)
+      element.classList.remove('country-sanctioned');
+      element.classList.add('country-116');
 
-    // Замена ссылок на рикролл
-    document.querySelectorAll('a[href="/blog/CHESScom/on-the-invasion-of-ukraine"]').forEach(link => {
-        link.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-    });
+      // Рикролл
+      element.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 
-    // Замена текста в tooltip
-    document.querySelectorAll("[v-tooltip='Нажать, чтобы узнать наше отношение к войне в Украине']").forEach(element => {
+      // Замена текста в tooltip
+      if (element.hasAttribute('v-tooltip')) {
         element.setAttribute('v-tooltip', 'Россия');
+      }
+
+      element.style.cursor = 'default';
     });
 }
 
@@ -23,16 +25,15 @@ function replaceFlagsAndEditLinks() {
  * Наблюдатель для отслеживания изменений в DOM и применения замен.
  */
 function observeDOMChanges() {
-    const targetNode = document.body;
-    const config = { childList: true, subtree: true };
+  const targetNode = document.body;
+  const config = { childList: true, subtree: true };
 
-    const observer = new MutationObserver(() => {
-        replaceFlagsAndEditLinks();
-    });
+  const observer = new MutationObserver(() => {
+    replaceFlagsAndEditLinks();
+  });
 
-    observer.observe(targetNode, config);
+  observer.observe(targetNode, config);
 }
 
-// Запуск функций
-replaceFlagsAndEditLinks();
-observeDOMChanges();
+replaceFlagsAndEditLinks(); // Выполняем замену флагов
+observeDOMChanges(); // Запускаем MutationObserver
